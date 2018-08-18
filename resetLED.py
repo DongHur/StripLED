f555b3ed8588a048ab38b536735cf526fca32f60
Dong Hur <dhur@college.harvard.edu>
2018-08-17 22:09:54 -0600

    updates files

--- resetLED.py
+++ resetLED.py
@@ -1,5 +1,6 @@
 from neopixel import *
-LED_COUNT      = 16      # Number of LED pixels.
+import time
+LED_COUNT      = 60      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
